02adc76c919e62b0afdf7d1d554a055dae9c78e5
Camilo Rodriguez <camilo.r@globant.com>
2021-09-14 16:19:16 -0500

    Init client-app

---
+++ client-app/src/app/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app-head test">head</div>
+      <div className="app-menu test">menu</div>
+      <div className="app-content test">content</div>
+      <footer className="app-footer test">footer</footer>
+    </div>
+  )
+}
+
+export default App
